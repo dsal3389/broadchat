2f6610cfb37cf6907f6f15a0e0d44cdd2f9264fb
Daniel Sonbolian <dsal3389@gmail.com>
2021-10-23 17:05:16 +0300

    ansi functions
    added back removed ansi preprocessor functions

--- src/screen.h
+++ src/screen.h
@@ -1,5 +1,38 @@
 #pragma once
 
+// ANSI escape sequences code + code names
+#define ESC "\033["
+#define EL "K"
+#define ED "J"
+#define UP "A"
+#define DN "B"
+#define LT "D"
+#define RT "C"
+#define CSAVE "s"
+#define CRESTORE "u"
+#define SD "T"
+#define SU "S"
+#define DSR "6n"
+#define HVP "f"
+#define OSC "\x9D"
+#define COLR "m"
+
+#define CURSOR_POS(x, y) printf(ESC "%d;%d" HVP, x, y)
+#define MOVE_CURSOR_UP(n) printf(ESC "%d" UP, n)
+#define MOVE_CURSOR_DN(n) printf(ESC "%d" DN, n)
+#define MOVE_CURSOR_LT(n) printf(ESC "%d" LT, n)
+#define MOVE_CURSOR_RT(n) printf(ESC "%d" RT, n)
+#define SCROLL_DOWN(n) printf(ESC "%d" SD, n)
+#define SCROLL_UP(n) printf(ESC "%d" SU, n)
+#define CLEAN_LINE printf(ESC "2" EL)
+#define CLEAN_ALL printf(ESC "2" ED)
+#define CLEAN_ALL_FROM_CURSOR printf(ESC ED)
+#define CLEAN_ALL_TO_CURSOR printf(ESC "1" ED)
+#define CURSOR_SAVE printf(ESC CSAVE)
+#define CURSOR_RET printf(ESC CRESTORE)
+#define CURSOR_REP printf(ESC DSR)
+#define SET_OSC printf(ESC OSC)
+#define SET_COLOR_CODE(n) printf(ESC "%d" COLR, n) 
 
 #define MESSAGES_BUFFER 110
 
